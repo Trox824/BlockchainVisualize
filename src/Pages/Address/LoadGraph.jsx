37fea13f26e243911df539cbe0fede8d34dbf0d0
Trox824 <nthun2k4@gmail.com>
2023-09-27 14:20:23 +1000

    no cmt

--- src/Pages/Address/LoadGraph.jsx
+++ src/Pages/Address/LoadGraph.jsx
@@ -13,6 +13,21 @@ import {
 import "@react-sigma/core/lib/react-sigma.min.css";
 import { GraphDefault } from "./GraphDefault";
 import GraphEvent from "./GraphEvent";
+import { truncateLabel } from "./truncateLabel";
+import axios from "axios";
+import {
+  NodeContext,
+  NodeContextProvider,
+  UseNodeContext,
+} from "../Address/NodeContext";
+const colorPalette = [
+  "#3F2021", // DARK BROWN
+  "#B04A5A", // DEEP CARMINE PINK
+  "#BA5B3F", // COPPER RUST
+  "#CB9576", // COPPER RUST
+  "#7FA0AC", // BLUE GRAY
+  "#EEE5D3", // ISABELLINE
+];
 export const FA2Graph = () => {
   return (
     <SigmaContainer className="relative w-full h-full bg-white rounded-xl">
